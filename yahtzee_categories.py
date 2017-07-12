605958589039921e90b2d8850bb284b1da5781f2
Owen Hunt <ophunt2000@gmail.com>
2017-07-12 10:35:43 -0500

    Created categories module Added upper section scoring functions

---
+++ yahtzee_categories.py
@@ -0,0 +1,18 @@
+def ones(d):
+	return 1*d.count(1)
+
+def twos(d):
+	return 2*d.count(2)
+
+def threes(d):
+	return 3*d.count(3)
+
+def fours(d):
+	return 4*d.count(4)
+
+def fives(d):
+	return 5*d.count(5)
+
+def sixes(d):
+	return 6*d.count(6)
+
