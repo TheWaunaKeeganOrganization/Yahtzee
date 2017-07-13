dd9ebd21ff77d153688465d9be57d181445bac7a
changwonchoi <cchang0705@gmail.com>
2017-07-13 13:44:49 -0500

    Chang (#5)
    * Add files via upload
    
    Fixed the bugs in keeps and assign.
    
    * Add files via upload

--- yahtzee_interface.py
+++ yahtzee_interface.py
@@ -10,8 +10,9 @@ class Player:
 		
 	def roll(self):
 		self.table = list(random.choice(list(itertools.product(range(1,7),repeat = len(self.table)))))
-		self.table += self.hand
-		self.hand = []
+		if len(self.hand) != 0:
+			self.table += self.hand
+			self.hand = []
 		print self.table
 
 	def keep(self, keepers):
@@ -20,7 +21,7 @@ class Player:
 			self.hand.append(n)
 
 	def assign(self):
-		print self.scorecard
+		for i in self.scorecard.keys(): print i, ":", self.scorecard[i], "\n"
 		assignment = raw_input('To which category do you wish to assign them (see the above scorecard):\n')
 		if assignment in self.scorecard.emptySpace():
 			self.scorecard.update(assignment, self.table)
@@ -30,8 +31,14 @@ class Player:
 
 	def keeps(self):
 		print self.table
-		inputted = raw_input('Which dice do you want to keep (comma separated numbers): \n').split(',')
-		k = map(lambda x: int(x) if x in "123456" else "Fail",inputted)
+		input = raw_input('Which dice do you want to keep (ex. for a roll of [5,4,3,4,6], you could write 4,4): \n')
+		try :
+			if len(input) > 0:
+				k = map(int, input.split(','))			
+			else: k = []
+		except:
+				print 'That is not a valid input'
+				return self.keeps()
 		tab = copy.deepcopy(self.table)
 		for n in k:
 			if n in tab:
@@ -42,22 +49,22 @@ class Player:
 		return k
 
 	def move(self):
-		raw_input('Input anything to roll\n')
-		self.roll()
-		assignyn = raw_input('Do you want to assign your points to a category? y/n:\n')
-		if assignyn == 'y' or assignyn == 'Y':
-			self.assign()
-			return None
-		keeping = self.keeps()
-		self.keep(keeping)
-		raw_input('Input anything to roll\n')
-		self.roll()
-		assignyn = raw_input('Do you want to assign your points to a category? y/n:\n')
-		if assignyn == 'y' or assignyn == 'Y':
-			self.assign()
-			return None
-		keeping = self.keeps()
-		self.keep(keeping)
-		raw_input('Input anything to roll\n')
-		self.roll()
+		rolls = 0
+		assigned = False
+		while rolls < 3 and not assigned:
+			raw_input('Input anything to roll\n')
+			self.roll()
+			rolls += 1
+			if rolls < 3:
+				assignyn = raw_input('Do you want to assign your points to a category? y/n:\n')
+				while assignyn not in ['y', 'Y', 'n', 'N']:
+						print 'That is not a valid input'
+						assignyn = raw_input('Do you want to assign your points to a category? y/n:\n')
+				if assignyn == 'y' or assignyn == 'Y':
+					self.assign()
+					assigned = True
+					return None
+				else:
+					keeping = self.keeps()
+					self.keep(keeping)
 		self.assign()
